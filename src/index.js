8d459ddcb2141260668d099bd3e74e3f5282acff
Patryk Nawolski <patryknawolski@gmail.com>
2019-03-23 00:25:28 +0100

    Remove redundant comment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import 'normalize.css'
 function component () {
   let element = document.createElement('h1')
 
-  // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = 'Hello world'
 
   return element
