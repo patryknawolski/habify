127f6081a4255748a63d06ccf5c828f414663794
Patryk Nawolski <patryknawolski@gmail.com>
2019-03-22 20:52:47 +0100

    Lint .js files

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   entry: './src/index.js',
@@ -6,4 +6,4 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   }
-};
+}
